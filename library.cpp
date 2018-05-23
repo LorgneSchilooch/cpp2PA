3a64ef84b6a76772fbca307a5301d9e6711f260e
Olivier Qut <tripalium@gmail.com>
2018-05-23 17:44:28 +0200

    Perceptron Simple - Classification

--- library.cpp
+++ library.cpp
@@ -1,7 +1,98 @@
 #include "library.h"
-
 #include <iostream>
+#include <cmath>
+#include <vector>
 
-void hello() {
-    std::cout << "Hello, World!" << std::endl;
+extern "C" {
+
+double *createLinearModel() {
+    double *w = new double[3];
+    return w;
 }
+
+void deleteLinearModel(double *w) {
+    delete w;
+}
+
+
+void trainLinearModel(double *w, double x[], double y[], double result[], double alpha, int len, bool rosenblatt) {
+
+    MatrixXd xMat(len, 3);
+    for (int i = 0; i < len; i++) {
+        xMat.row(i).col(0) << 1;
+        xMat.row(i).col(1) << x[i];
+        xMat.row(i).col(2) << y[i];
+    }
+
+    MatrixXd resultMat(len, 1);
+
+    for (int i = 0; i < len; i++) {
+        resultMat.row(i).col(0) << result[i];
+    }
+
+    dllTrainLinearModel(w, xMat, resultMat, alpha, rosenblatt);
+}
+
+int predictLinearModel(double *w, double x, double y) {
+
+    VectorXd xp(3);
+    VectorXd wp(3);
+    xp(0) = 1;
+    xp(1) = x;
+    xp(2) = y;
+    wp(0) = w[0];
+    wp(1) = w[1];
+    wp(2) = w[2];
+    return dllPredictLinearModel(wp, xp);
+
+}
+
+}
+
+// ######################### Fonctions internes #########################
+
+
+void dllTrainLinearModel(double *w, MatrixXd x, MatrixXd result, double alpha, bool choice) {
+
+    VectorXd wResult(3);
+    wResult(0) = 1;
+    wResult(1) = 1;
+    wResult(2) = 1;
+    wResult << dllLearningLinearModel(wResult, x, result, alpha, choice);
+    w[0] = wResult(0);
+    w[1] = wResult(1);
+    w[2] = wResult(2);
+}
+
+MatrixXd dllLearningLinearModel(VectorXd w, MatrixXd x, MatrixXd result, double alpha, bool choice) {
+
+    VectorXd xt(x.cols());
+    for (int i = 0; i < x.rows(); i++) {
+        xt = x.row(i);
+        if (!choice) {
+            while (dllPredictLinearModel(xt, w) != result.coeff(i, 0)) {
+                w(0) = w(0) + alpha * result.coeff(i, 0) * xt(0);
+                w(1) = w(1) + alpha * result.coeff(i, 0) * xt(1);
+                w(2) = w(2) + alpha * result.coeff(i, 0) * xt(2);
+            }
+        } else {
+            w(0) = w(0) + alpha * (result.coeff(i, 0) - dllPredictLinearModel(xt, w)) * xt(0);
+            w(1) = w(1) + alpha * (result.coeff(i, 0) - dllPredictLinearModel(xt, w)) * xt(1);
+            w(2) = w(2) + alpha * (result.coeff(i, 0) - dllPredictLinearModel(xt, w)) * xt(2);
+        }
+    }
+
+    return w;
+}
+
+bool testAlgorithm(VectorXd w, VectorXd x, int result) {
+
+    return dllPredictLinearModel(w, x) == result;
+
+};
+
+int dllPredictLinearModel(VectorXd w, VectorXd x) {
+    return (w.transpose() * x).determinant() < 0 ? -1 : 1;
+};
+
+
--- library.h
+++ library.h
@@ -1,6 +1,22 @@
 #ifndef CPP2PA_LIBRARY_H
 #define CPP2PA_LIBRARY_H
 
-void hello();
+#include "Eigen/Dense"
+
+using namespace Eigen;
+using namespace std;
+
+void dllTrainLinearModel(double *, MatrixXd, MatrixXd, double, bool);
+
+MatrixXd dllLearningLinearModel(VectorXd, MatrixXd, MatrixXd, double, bool);
+
+bool testAlgorithm(MatrixXd, MatrixXd, int result);
+
+int dllPredictLinearModel(VectorXd, VectorXd);
+
+
+#endif
+
+
 
 #endif
